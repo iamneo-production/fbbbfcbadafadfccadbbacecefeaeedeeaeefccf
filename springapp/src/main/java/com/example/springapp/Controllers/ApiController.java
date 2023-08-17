f0becfb8b6693bd95aba64a0df1b20b4518be2d6
Iamneo <techadmin@iamneo.ai>
2023-08-17 03:59:17 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
+++ springapp/src/main/java/com/example/springapp/Controllers/ApiController.java
@@ -1,8 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class ApiController {
-    @GetMapping("/welcome")
-    public String Display()
-    {
-        return "Welcome";
-    }
+public class MyDay1App {
+	@GetMapping("/welcome")
+	public String display()
+	{
+		return "Welcome";
+	}
 }
